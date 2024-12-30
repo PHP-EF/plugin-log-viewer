6e3644efebc462c2f70ca6cb2ce0775c3dfcb960
Jamie Donaldson <jamie.donaldson@mamasandpapas.com>
2024-12-30 11:43:08 +0000

    uncommented out the }

--- api.php
+++ api.php
@@ -6,7 +6,7 @@ $app->get('/plugin/logviewer/settings', function ($request, $response, $args) {
 	$logviewerPlugin = new logviewerPlugin();
 	 if ($logviewerPlugin->auth->checkAccess('ACL-Plugin-LogViewer')) {
 		$logviewerPlugin->api->setAPIResponseData($logviewerPlugin->_pluginGetSettings());
-	// }
+	}
 	$response->getBody()->write(jsonE($GLOBALS['api']));
 	return $response
 		->withHeader('Content-Type', 'application/json;charset=UTF-8')
